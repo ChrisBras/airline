d4c671749b692f1790e7830955b26982d4c51875
Chris Bras <ceb922@hotmail.com>
2013-03-20 21:59:04 -0400

    changed migration file

--- db/migrate/20130320213011_add_something_to_cargos.rb
+++ db/migrate/20130320213011_add_something_to_cargos.rb
@@ -1,5 +1,13 @@
 class AddSomethingToCargos < ActiveRecord::Migration
   def change
-    add_column :cargos, :flight_id, :integer
+    create_table :cargos do |t|
+      t.integer :weight
+      t.string :contents
+      t.references :flight
+
+      t.timestamps
+    end
+    add_index :cargos, :flight_id
   end
 end
+
