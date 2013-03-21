1afef88893e64828d9f6971a15d57b8dcbe8dd3d
Chris Bras <ceb922@hotmail.com>
2013-03-20 23:51:45 -0400

    i think i got the sass working

---
+++ app/assets/stylesheets/config.rb
@@ -0,0 +1,9 @@
+require 'bootstrap-sass'
+http_path = "/"
+css_dir = "."
+sass_dir = "."
+images_dir = "img"
+javascripts_dir = "js"
+output_style = :compressed
+relative_assets=true
+line_comments = false
