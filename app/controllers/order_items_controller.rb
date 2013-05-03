731edfbf0ae43e100e5d267e026b81323ad4a5f3
Chris Bras <ceb922@hotmail.com>
2013-05-03 12:08:07 -0400

    removed html redirect

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -11,8 +11,7 @@ class OrderItemsController < ApplicationController
   @cargo_item = CargoItem.new(order_id: params[:order_id], cargo_id: @cargo_id)
      respond_to do |format|
         if @cargo_item.save
-          format.html { redirect_to @cargo_item, notice: 'Cargo item was successfully created.' }
-          format.json { render json: @cargo_item, status: :created, location: @cargo_item }
+          format.json { render json: @cargo_item, status: :created }
         else
           format.json { render json: @cargo_item.errors, status: :unprocessable_entity }
         end
